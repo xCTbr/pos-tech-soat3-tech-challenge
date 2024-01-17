3e68dc84647ed5a323eb287a82cd5ff0a5609fdd
Bruna Carlota <bru.carlota@yahoo.com.br>
2024-01-16 23:13:26 -0300

    Add main route /

--- src/frameworks_drivers/api/routes/index.js
+++ src/frameworks_drivers/api/routes/index.js
@@ -1,5 +1,6 @@
 import customerRoutes from "./customerRoutes.js";
 
 export default function routes(app, express) {
-  app.use('/api/v1/customers', customerRoutes(express));
+	app.route("/").get((req, res) => res.status(200).send("Sistema de pedidos"));
+  app.use('/customers', customerRoutes(express));
 }
