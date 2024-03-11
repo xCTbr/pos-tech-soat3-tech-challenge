3f3ab1ee56fed459a81b01594d6e59cb99e1a9c9
Bruna Carlota <bru.carlota@yahoo.com.br>
2024-03-11 13:50:40 -0300

    update webhook token

--- src/config/webhookConfig.js
+++ src/config/webhookConfig.js
@@ -1,3 +1,3 @@
-// token atual 0542895c-7735-41b7-bfcb-de50fe59b2c9
-// whcli forward --token=0542895c-7735-41b7-bfcb-de50fe59b2c9 --api-key=0542895c-7735-41b7-bfcb-de50fe59b2c9 --target=http://localhost:3000/webhook
-export const webhookURL = 'https://webhook.site/0542895c-7735-41b7-bfcb-de50fe59b2c9';
+// token atual 283d9c44-b4d5-4d3b-b4b9-96a8ab11a870
+// whcli forward --token=283d9c44-b4d5-4d3b-b4b9-96a8ab11a870 --api-key=283d9c44-b4d5-4d3b-b4b9-96a8ab11a870 --target=http://localhost:3000/webhook
+export const webhookURL = 'https://webhook.site/283d9c44-b4d5-4d3b-b4b9-96a8ab11a870';
